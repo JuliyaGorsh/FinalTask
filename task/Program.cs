2edb50429367ed463623748cdbdcf76e4eeb7365
Yuliya <kuv_96@mail.ru>
2022-11-14 03:35:46 +0300

    Method CountLess3

---
+++ task/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int CountLess3(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) count++;
+    }
+    return count;
+}
+
